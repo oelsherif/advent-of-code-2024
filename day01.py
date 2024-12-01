75076a83e6538a274f9550f05af0b0f35ee0a4f5
Omar Elsherif <omar343@gmail.com>
2024-12-01 16:23:31 -0500

    update day01

--- day01.py
+++ day01.py
@@ -1,16 +1,10 @@
 from collections import Counter
 
 with open("inputs/01.txt", "r") as File:
-    lines = File.readlines()
+    lines = [line.split() for line in File.readlines()]
 
-arr1, arr2 = [], []
-for line in lines:
-    a, b = line.split()
-    arr1.append(int(a))
-    arr2.append(int(b))
-
-arr1.sort()
-arr2.sort()
+arr1 = sorted(int(line[0]) for line in lines)
+arr2 = sorted(int(line[1]) for line in lines)
 
 ans_p1 = sum(abs(a-b) for a, b in zip(arr1, arr2))
 print(f"Part 1 answer: {ans_p1}")
