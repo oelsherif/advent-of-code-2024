7643253f2ba14f01d62c7c5c03109e7abeeb07db
Omar Elsherif <omar343@gmail.com>
2024-12-10 15:01:22 -0500

    submit day10

--- day10.py
+++ day10.py
@@ -1,15 +1,9 @@
 from collections import defaultdict
-with open("inputs/10_test.txt", "r") as File:
+with open("inputs/10.txt", "r") as File:
     grid = [[-1] + [int(char) for char in line[:-1]] + [-1] for line in File]
 
 n_cols = len(grid[0])
 grid = [[-1] * n_cols] + grid + [[-1] * n_cols]
-
-here = defaultdict(int)
-for y, row in enumerate(grid):
-    for x, num in enumerate(row):
-        if num == 0:
-            here[(x, y)] += 1
 
 deltas = [
     (0, -1),
@@ -18,5 +12,38 @@ deltas = [
     (-1, 0),
 ]
 
-for next_num in range(1, 10):
+here = defaultdict(set)
+for y, row in enumerate(grid):
+    for x, num in enumerate(row):
+        if num == 9:
+            here[(x, y)] |= {(x,y)}
+
+for prev_height in range(8, -1, -1):
+    prev = defaultdict(set)
+    for (x, y), peaks in here.items():
+        for dx, dy in deltas:
+            new_x, new_y = x+dx, y+dy
+            if grid[new_y][new_x] == prev_height:
+                prev[(new_x, new_y)] |= peaks
+    here = prev.copy()
+
+ans_p1 = sum([len(peak_list) for peak_list in here.values()])
+print(f"Part 1 answer: {ans_p1}")
+
+here = defaultdict(int)
+for y, row in enumerate(grid):
+    for x, num in enumerate(row):
+        if num == 0:
+            here[(x, y)] += 1
+
+for next_height in range(1, 10):
     next = defaultdict(int)
+    for (x, y), count in here.items():
+        for dx, dy in deltas:
+            new_x, new_y = x+dx, y+dy
+            if grid[new_y][new_x] == next_height:
+                next[(new_x, new_y)] += count
+    here = next.copy()
+
+ans_p2 = sum(here.values())
+print(f"Part 2 answer: {ans_p2}")
