9f462de2bd695a6cffbcf597f2769fdd7aecbbc8
oelsherif <omar343@gmail.com>
2024-12-21 08:31:33 -0500

    in_progress day21

--- day21.py
+++ day21.py
@@ -26,16 +26,61 @@ def get_numpad_presses(s):
         x1, y1 = numpad_loc[prev_char]
         x2, y2 = numpad_loc[char]
         if y2 > y1:
-            presses += '>' * (y2-y1)
+            if x2 > x1:
+                presses += '>' * (x2-x1)
+            else:
+                presses += '<' * (x1-x2)
+            presses += 'v' * (y2-y1)
         else:
             presses += '^' * (y1-y2)
-        if x2 > x1:
-            presses += '>' * (x2-x1)
-        else:
-            presses += '<' * (x1-x2)
+            if x2 > x1:
+                presses += '>' * (x2-x1)
+            else:
+                presses += '<' * (x1-x2)
         presses += 'A'
         prev_char = char
     return presses
 
-#print(get_numpad_presses('029A'))
-        
+def get_keypad_presses(s, n):
+    presses = ''
+    prev_char = 'A'
+    for char in s:
+        x1, y1 = keypad_loc[prev_char]
+        x2, y2 = keypad_loc[char]
+        if y2 > y1:
+            presses += 'v' * (y2-y1)
+            if x2 > x1:
+                presses += '>' * (x2-x1)
+            else:
+                presses += '<' * (x1-x2)
+        else:
+            if x2 > x1:
+                presses += '>' * (x2-x1)
+            else:
+                presses += '<' * (x1-x2)
+            presses += '^' * (y1-y2)
+        presses += 'A'
+        prev_char = char
+    if n:
+        return get_keypad_presses(presses, n-1)
+    return presses
+
+ans_p1 = 0
+for code in codes:
+    s1 = get_numpad_presses(code)
+    s2 = get_keypad_presses(s1, 1)
+
+t1 = time.time()
+ans_p1 = 0
+for code in codes:
+    s1 = get_numpad_presses(code)
+    s2 = get_keypad_presses(s1, 1)
+    code_int = int(code[:-1])
+    len2 = len(s2)
+    print(code_int, len2)
+    print(s2)
+    ans_p1 += int(code[:-1]) * len(s2)
+t2 = time.time()
+
+print(f"Part 1 answer: {ans_p1}")
+print(f"Part 1 time: {t2 - t1:.3f}s")
