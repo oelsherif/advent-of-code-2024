0aa01ca3451e7ad0e1b9def119758112cd5e14e7
Omar Elsherif <omar343@gmail.com>
2024-12-22 12:11:46 -0500

    submit day22

--- day22.py
+++ day22.py
@@ -1,4 +1,5 @@
 import time
+from collections import defaultdict
 with open("inputs/22.txt", "r") as File:
     secrets = [int(line) for line in File]
 
@@ -17,13 +18,37 @@ def step_3(num):
     num %= 16777216
     return num
 
-def evolve_secret(num, n = 1):
-    for _ in range(n):
-        num = step_3(step_2(step_1(num)))
-    return num
+def evolve_secret(num):
+    return step_3(step_2(step_1(num)))
 
 t1 = time.time()
-ans_p1 = sum(evolve_secret(num, 2000) for num in secrets)
-t2 = time.time()
+secret_changes = []
+secret_evolution = []
+for num in secrets:
+    changes = []
+    evolution = []
+    for _ in range(2000):
+        new_num = evolve_secret(num)
+        evolution.append(new_num)
+        change = new_num%10 - num%10
+        changes.append(change)
+        num = new_num
+    secret_changes.append(changes)
+    secret_evolution.append(evolution)
+
+ans_p1 = sum(sequence[-1] for sequence in secret_evolution)
 print(f"Part 1 answer: {ans_p1}")
-print(f"Part 1 time: {t2 - t1:.3f}s")
+
+sequence_bananas = defaultdict(int) #total number of bananas for sequence
+for (changes, evolution) in zip(secret_changes, secret_evolution):
+    sequences = set()
+    for j in range(4, len(changes)):
+        sequence = tuple(changes[j-4:j])
+        if sequence in sequences:
+            continue
+        sequences.add(sequence)
+        sequence_bananas[sequence] += evolution[j-1]%10
+ans_p2 = max(sequence_bananas.values())
+t2 = time.time()
+print(f"Part 2 answer: {ans_p2}")
+print(f"Time: {t2 - t1:.3f}s")
