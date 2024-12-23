e0b74b6e34f26d0365571fbfce6fdee0a95c2ed2
Omar Elsherif <omar343@gmail.com>
2024-12-23 05:47:26 -0500

    submit day23

--- day23.py
+++ day23.py
@@ -4,17 +4,17 @@ with open("inputs/23.txt", "r") as File:
     lines = File.readlines()
 
 t1 = time.time()
-friends = defaultdict(set)
+connections = defaultdict(set)
 for line in lines:
     c1, c2 = line[0:2], line[3:5]
-    friends[c1].add(c2)
-    friends[c2].add(c1)
+    connections[c1].add(c2)
+    connections[c2].add(c1)
 
 groups = set()
-for comp1, friends1 in friends.items():
+for comp1, friends1 in connections.items():
     if comp1[0] == 't':
         for comp2 in friends1:
-            friends2 = friends[comp2]
+            friends2 = connections[comp2]
             comps = friends1 & friends2
             for comp3 in comps:
                 groups.add(tuple(sorted([comp1, comp2, comp3])))
@@ -24,4 +24,18 @@ t2 = time.time()
 print(f"Part 1 answer: {ans_p1}")
 print(f"Part 1 time: {t2 - t1:.3f}s")
 
+groups = set(tuple([comp]) for comp in connections.keys())
+while len(groups) > 1:
+    new_groups = set()
+    for group in groups:
+        friends_list = [connections[comp] for comp in group]
+        common_friends = set.intersection(*friends_list)
+        for friend in common_friends:
+            new_groups.add(tuple(sorted(list(group) + [friend])))
+    groups = new_groups.copy()
+party = list(list(groups)[0])
 
+ans_p2 = ','.join(party)
+t3 = time.time()
+print(f"Part 2 answer: {ans_p2}")
+print(f"Part 2 time: {t3 - t2:.3f}s")
